d24e741829061117b4e43618bcd04b55058466dc
Ron Miller <rmiller232501@gmail.com>
2024-05-21 02:58:16 -0400

    Setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from "react-router-dom";
 import './index.css'
+
+import ROUTER from './router/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ROUTER} />
   </React.StrictMode>,
 )
---
+++ src/router/routes.tsx
@@ -0,0 +1,11 @@
+import App from "../App.tsx";
+
+const ROUTES = [
+  {
+    path: "/",
+    element: <App />,
+  },
+]
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default ROUTES;
